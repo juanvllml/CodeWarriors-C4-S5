42b7abc435f0d1476de7782353ef11baac41b3ba
Juan Villamil <juanvllml@users.noreply.github.com>
2021-12-21 09:01:39 -0500

    Upload backend to Heroku

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,8 @@
 import { server } from './server';
 import './database';
 
-server.start({port: 3100}, ({port}) => {
+const listen_port = process.env.PORT || 5100
+
+server.start({port: listen_port}, ({port}) => {
     console.log('Server on port', port);
 })
